4dd62297d130af40cf31e3406c322f37c9a059ab
Harpreet Kaur <hkaur22@jaumebalmes.net>
2024-01-05 19:49:13 +0100

    Part0 done, imprime los nombres pedidos en el enunciado

--- Projecte Arrays/js/script.js
+++ Projecte Arrays/js/script.js
@@ -1,54 +1,100 @@
 			
-// POKEMONS
 
+//Declaracion de variables globales
 let dades;
-
+let pokemonsNoms = [];
+let municipisNoms = [];
+let meteoritesNoms = [];
+let moviesNoms = [];
 
 // POKEMONS
-fetch("js/data/pokemon.json")
-.then((response) => response.json())
-.then((data) => {
-	dades = data.pokemon;		
-	
-	console.log(dades)
-	console.log(dades[0].name)
+function datosPokemon(){
+	fetch("js/data/pokemon.json")
+	.then((response) => response.json())
+	.then((data) => {
+		dades = data.pokemon;		
 
-});
+		
+		for(let i= 0; i<dades.length; i++){
+			let pokemonNuevo = dades[i];
+			let nombre = pokemonNuevo.name;
+			console.log(i + " " + nombre);
+			pokemonsNoms.push(nombre);
+		};
+		
+		console.table(pokemonsNoms);
+		// console.log("Array con los nombre de los pokemons: " + "br" + pokemonsNoms);
+		//--> esto imprime todos los datos de pokemon.
+		// console.log(dades) 
+		// console.log(dades[0].name)
+		// console.log(dades[0]);
 
+	});
+}
 
-
-/*
 // MUNICIPIS
-fetch("js/data/municipis.json")
-.then((response) => response.json())
-.then((data) => {
-	dades = data.elements;		
-	
-	console.log(dades)
-	console.log(dades[0].municipi_nom)
-});
+function datosMunicipis(){
+	fetch("js/data/municipis.json")
+	.then((response) => response.json())
+	.then((data) => {
+		dades = data.elements;		
 
-/*
+		
+		for(let i= 0; i<dades.length; i++){
+			let municipi = dades[i];
+			let nom = municipi.municipi_nom;
+			console.log(i + " " + nom);
+			municipisNoms.push(nom);
+		}
+		console.table(municipisNoms);
+		
+		// console.log(dades)
+		// console.log(dades[0].municipi_nom)
+	});
+
+}
 
 // METEORITS
-fetch("js/data/earthMeteorites.json")
-.then((response) => response.json())
-.then((data) => {
-	dades = data;		
-	
-	console.log(dades)
-	console.log(dades[0].name)
-});
+function datosMeteorites() {
+  fetch("js/data/earthMeteorites.json")
+    .then((response) => response.json())
+    .then((data) => {
+      dades = data;
+
+	  
+	  for(let i=0; i<dades.length; i++){
+		let meteorite = dades[i];
+		let nomMeteorite = meteorite.name;
+		console.log( i + " " + nomMeteorite);
+		meteoritesNoms.push(nomMeteorite);
+	  }
+	  console.table(meteoritesNoms);
+    //   console.log(dades);
+    //   console.log(dades[0].name);
+    });
+}
 
 
 // MOVIES
-fetch("js/data/movies.json")
-.then((response) => response.json())
-.then((data) => {
-	dades = data.movies;		
-	
-	console.log(dades)
-	console.log(dades[0].title)
-});
+function datosMovies() {
+  fetch("js/data/movies.json")
+    .then((response) => response.json())
+    .then((data) => {
+      dades = data.movies;
 
-*/
+	  
+	  for(let i=0; i<dades.length; i++){
+		let movie = dades[i];
+		let nomMovie = movie.title;
+		console.log(i + " " + nomMovie);
+		moviesNoms.push(nomMovie);
+	  }
+	  console.table(moviesNoms);
+
+    //   console.log(dades);
+    //   console.log(dades[0].title);
+    });
+}
+
+
+
